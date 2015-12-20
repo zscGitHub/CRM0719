543e9306ab2b78f48fe955ff9a78a2a5ea4fc770
zscGitHub <1026192404@qq.com>
2015-12-20 23:52:59 +0800

    gitHub server 2
    2

--- src/com/zsc/gitTest/Test01.java
+++ src/com/zsc/gitTest/Test01.java
@@ -5,6 +5,7 @@ public class Test01 {
 	public static void main(String[] args) {
 		System.out.println("hello gitHub 服务器");
 		System.out.println(" gitHub 服务器 response to local");
+		System.out.println(" gitHub 服务器 response to local 22");
 	}
 
 }
