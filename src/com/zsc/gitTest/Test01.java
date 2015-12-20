5db87c858b76db0fba06b9e1fbebf6fa00b6c4d2
zscGitHub <1026192404@qq.com>
2015-12-20 23:20:13 +0800

    update file 02

--- src/com/zsc/gitTest/Test01.java
+++ src/com/zsc/gitTest/Test01.java
@@ -3,8 +3,7 @@ package com.zsc.gitTest;
 public class Test01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("hello gitHub 服务器");
 	}
 
 }
